c43d172bd63714e9a91d441ebb5bd68bc2ab9835
Pavel Gilyov <GrandArchTemplar@gmail.com>
2024-04-08 22:02:38 +0300

    Update xmonad.hs

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -5,22 +5,56 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-main :: IO()
-main = xmonad =<< xmobar myConfig 
+main :: IO ()
+main = xmonad =<< xmobar myConfig
 
-myConfig = def 
-  { modMask = mod4Mask 
-  , terminal = "alacritty"
-  , workspaces = myWorkspaces
-  } `additionalKeys` myKeys
+myConfig =
+  def
+    { modMask = myModMask
+    , terminal = myTerminal
+    , workspaces = myWorkspaces
+    , normalBorderColor = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , handleExtraArgs = myHandleExtraArgs
+    , layoutHook = myLayout
+    }
+    `additionalKeys` myKeys
 
-myWorkspaces = ["1:main", "2:web", "3:tg", "4", "5", "6", "7", "8", "9"]
+myWorkspaces = ["main", "web", "tg", "4", "5", "6", "7", "8", "9"]
 
-myKeys = 
-  [ ((mod1Mask , xK_Shift_L          ), spawn keyboard_switch)
-  , ((shiftMask, xK_Alt_L            ), spawn keyboard_switch)
-  , ((controlMask .|. mod4Mask , xK_l), spawn lockscreen)
+myHandleExtraArgs xs theConf = case xs of
+  [] -> return theConf
+  _ -> fail ("unrecognized flags:" ++ show xs)
+
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+myTerminal :: String
+myTerminal = "alacritty"
+
+myNormalBorderColor :: String
+myNormalBorderColor = "gray"
+
+myFocusedBorderColor :: String
+myFocusedBorderColor = "green"
+
+myKeys =
+  [ ((mod1Mask, xK_Shift_L), spawn keyboardSwitch)
+  , ((shiftMask, xK_Alt_L), spawn keyboardSwitch)
+  , ((controlMask .|. myModMask, xK_l), spawn lockscreen)
+  , ((myModMask .|. shiftMask, xK_s), spawn screenshot)
   ]
 
-lockscreen = "i3lock-color -c 181818 -e -k --time-color=cfcfcf --date-color=cfcfcf"
-keyboard_switch = "~/.config/xmonad/keyboard_switch.sh"
+defaultLayout = tiled ||| Mirror tiled ||| Full
+ where
+  tiled = Tall nmaster delta ratio
+  nmaster = 1
+  ratio = 1 / 2
+  delta = 3 / 100
+
+myLayout = onWorkspace "main" Full . onWorkspace "tg" Full $ defaultLayout
+
+
+lockscreen = "i3lock-color -c 181818 -e -k --time-color=cfcfcf --layout-color=cfcfcf --date-color=cfcfcf --date-str=\"%A: %d/%m/%Y\" --keylayout 0 --radius 120"
+keyboardSwitch = "~/.config/xmonad/keyboard_switch.sh"
+screenshot = "maim -s | xclip -selection clipboard -t image/png"
